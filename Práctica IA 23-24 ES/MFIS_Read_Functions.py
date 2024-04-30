8fd38b48154ac48c7d18091c799355be2ec2e414
Pablo <100451061@alumnos.uc3m.es>
2024-04-30 14:41:42 +0200

    ahora podemos ver las graficas

--- Práctica IA 23-24 ES/MFIS_Read_Functions.py
+++ Práctica IA 23-24 ES/MFIS_Read_Functions.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+import matplotlib.pyplot as plt
 import numpy as np
 import skfuzzy as skf
+from skfuzzy import control as ctrl
 
 from MFIS_Classes import *
 
@@ -75,13 +77,59 @@ def readApplicationsFile():
     return applicationList
 
 
+# Al invocar la función los valores de x me hacen muy grandes hasta 100
+# Por lo tanto solo cojo un trozo de la función no sé si vale
+
+def inputs2():
+    Age = ctrl.Antecedent(np.arange(0, 101, 1), "Age")
+    IncomeLevel = ctrl.Antecedent(np.arange(0, 151, 1), "IncomeLevel")
+    Assets = ctrl.Antecedent(np.arange(0, 51, 1), "Assets")
+    Amount = ctrl.Antecedent(np.arange(0, 9, 1), "Amount")
+    Job = ctrl.Antecedent(np.arange(0, 6, 1), "Job")
+    History = ctrl.Antecedent(np.arange(0, 7, 1), "History")
+    # Tambien puedo lista=[] voy añadiendo los que no aparece en la lista y luego
+    # ejecuto la parte de abajo ir dibujando la gráfica si me dan un fichero y
+    # no sé los inputs
+
+    """{'Age=Young': <MFIS_Classes.FuzzySet object at 0x000001EC34266F10>"""
+    diccionario = readFuzzySetsFile("InputVarSets.txt")
+    # Creo cada una de las gráficas
+    for variables in diccionario.values():
+        if variables.var == "Age":
+            Age[str(variables.label)] = np.interp(Age.universe, variables.x, variables.y)
+        if variables.var == "IncomeLevel":
+            IncomeLevel[str(variables.label)] = np.interp(IncomeLevel.universe, variables.x, variables.y)
+        if variables.var == "Assets":
+            Assets[str(variables.label)] = np.interp(Assets.universe, variables.x, variables.y)
+        if variables.var == "Amount":
+            Amount[str(variables.label)] = np.interp(Amount.universe, variables.x, variables.y)
+        if variables.var == "Job":
+            Job[str(variables.label)] = np.interp(Job.universe, variables.x, variables.y)
+        if variables.var == "History":
+            History[str(variables.label)] = np.interp(History.universe, variables.x, variables.y)
+
+
+def outputs2():
+    diccionario = readFuzzySetsFile("Risks.txt")
+    Risk = ctrl.Consequent(np.arange(0, 101, 1), "Risk")
+
+    for variables in diccionario.values():
+        Risk[str(variables.label)] = np.interp(Risk.universe, variables.x, variables.y)
+
+    Risk.view()
+    plt.show()
+
+
 if __name__ == '__main__':
     # Cargar los conjuntos borrosos desde el archivo InputVarSets.txt
-    fuzzy_sets = readFuzzySetsFile('InputVarSets.txt')
-    print("Fuzzy Sets Loaded:")
-    fuzzy_sets.printFuzzySetsDict()
+    # fuzzy_sets = readFuzzySetsFile('InputVarSets.txt')
+    # print("Fuzzy Sets Loaded:")
+    # fuzzy_sets.printFuzzySetsDict()
 
     # Cargar las reglas desde el archivo Rules.txt
-    rules = readRulesFile('Rules.txt')
-    print("Reglas cargadas:")
-    rules.printRuleList()
+    # rules = readRulesFile('Rules.txt')
+    # print("Reglas cargadas:")
+    # rules.printRuleList()
+
+    inputs2()
+    outputs2()
